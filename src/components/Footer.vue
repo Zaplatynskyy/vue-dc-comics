5c292da331bef5d5a1e90e53d81b0571f8eb015d
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-13 15:59:55 +0100

    change favicon + layout page

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="debug">
+        <div class="top_footer">
+
+        </div>
+
+        <div class="main_footer">
+
+        </div>
+
+        <div class="bottom_footer">
+            
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name : 'Footer'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name : 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/css/variables.scss';
+
+    header {
+        height: 100px;
+        background-color: $bg_white;
+    }
+</style>
