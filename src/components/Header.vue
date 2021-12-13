8179a47c0c5aadaf3a28fbe22d9980126db1b452
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-13 16:37:18 +0100

    header logo and nav_bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,24 @@
 <template>
     <header>
-
+        <div class="container">
+            <a href="#">
+                <img src="../assets/img/dc-logo.png" alt="logo dc comics">
+            </a>
+            <nav class="header_nav">
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#" class="selected">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Collectibles</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
 
@@ -14,7 +32,49 @@ export default {
 @import '../assets/css/variables.scss';
 
     header {
-        height: 100px;
+        height: 120px;
         background-color: $bg_white;
+
+        .container {
+            height: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            img {
+                    height: 80px;
+            }
+
+            .header_nav {
+                
+                ul {
+                    list-style: none;
+
+                    li {
+                        display: inline-block;
+
+                        a {
+                            height: 120px;
+                            line-height: 120px;
+                            font-weight: bold;
+                            color: $text_link_color;
+                            text-transform: uppercase;
+                            display: inline-block;
+                            margin: 0 15px;
+
+                            &:hover {
+                                color: $primary_color;
+                                border-bottom: 5px solid $primary_color;
+                            }
+
+                            &.selected {
+                                color: $primary_color;
+                                border-bottom: 5px solid $primary_color;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 </style>
